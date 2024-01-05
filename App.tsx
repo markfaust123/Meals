9458485e89f103ad72e3a5888e40b29c68c6c067
Mark Faust <markfaust123@gmail.com>
2024-01-05 17:10:09 -0500

    finished styling the Drawer Navigator for all screens

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import MealsOverviewScreen from "./src/screens/MealsOverviewScreen";
 import MealDetailScreen from "./src/screens/MealDetailScreen";
 import FavoritesScreen from "./src/screens/FavoritesScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -21,6 +22,10 @@ const DrawerNavigator = () => {
           },
           headerTintColor: "white",
           sceneContainerStyle: { backgroundColor: "#3f2f25" },
+          drawerContentStyle: { backgroundColor: "#351401" },
+          drawerInactiveTintColor: "white",
+          drawerActiveTintColor: "#351401",
+          drawerActiveBackgroundColor: "#e4baa1",
         }}
       >
         <Drawer.Screen
@@ -28,9 +33,20 @@ const DrawerNavigator = () => {
           component={CategoriesScreen}
           options={{
             title: "All Categories",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="list" color={color} size={size} />
+            ),
           }}
         />
-        <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+        <Drawer.Screen
+          name="Favorites"
+          component={FavoritesScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="star" color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </>
   );
