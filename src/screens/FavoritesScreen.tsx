72ad6aa7e8a554bb5920385df5b0d255d04fe691
Mark Faust <markfaust123@gmail.com>
2024-01-06 17:13:20 -0500

    finished entire Context API state management implementation, now going to implement with Redux to practice an alternative method

--- src/screens/FavoritesScreen.tsx
+++ src/screens/FavoritesScreen.tsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import Meal from "../../models/meal";
 import { MEALS } from "../../data/dummy-data";
 import MealsList from "../components/MealsList/MealsList";
+import { StyleSheet, View, Text } from "react-native";
 
 const FavoritesScreen = () => {
   const favoriteMealsCtx = useContext(FavoritesContext);
@@ -11,8 +12,29 @@ const FavoritesScreen = () => {
   );
 
   return (
-    <MealsList meals={favoriteMeals} />
+    <>
+      {favoriteMeals && favoriteMeals.length ? (
+        <MealsList meals={favoriteMeals} />
+      ) : (
+        <View style={styles.rootContainer}>
+            <Text style={styles.text}>You have no favorite meals yet.</Text>
+        </View>
+      )}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1, 
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold",
+    }
+})
 
 export default FavoritesScreen;
